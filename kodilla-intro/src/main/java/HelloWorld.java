87edade804361e149206cd98b40e481735e9920c
Michal Wysocki <michal.wysocki.ta@gmail.com>
2026-01-03 19:03:01 +0100

    Typy Danych i Zmienne

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("HelloWorld!");
+        System.out.println("Michal");
+        System.out.println("31");
+        System.out.println("W");// [3]
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,21 @@
+public class MainExtended {                           // [1]
+    public static void main(String[] args) {// [2]
+        String introducing = "Hey, jestem Michal. Informacje o mnie:";
+        System.out.println(introducing);
+        System.out.println("Rok urodzenia");
+        int yearOfBirth = 1994;
+        System.out.println(yearOfBirth);
+        System.out.println("Miejsce urodzenia");
+        String cityOfBirth = "Suwalki";
+        System.out.println(cityOfBirth);
+        System.out.println("Palacy?");
+        boolean notSmoking = false;
+        System.out.println(notSmoking);
+        System.out.println("Wzrost");
+        double myHeight = 1.78;
+        System.out.println(myHeight);
+        System.out.println("Kategoria/e prawajazdy");
+        char myDrivningCategory = 'B';
+        System.out.println(myDrivningCategory);
+    }
+}
