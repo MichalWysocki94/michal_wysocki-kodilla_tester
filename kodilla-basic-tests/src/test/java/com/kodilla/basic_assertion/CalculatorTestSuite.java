481cd88729342664eed41f59e7f4581fafbf5c47
Michal Wysocki <michal.wysocki.ta@gmail.com>
2026-02-02 17:10:20 +0100

    Zadanie 3.6 Testy jednostkowy: odejmowanie, potegowanie

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTestSuite {
+
     @Test
     public void testSum() {
         Calculator calculator = new Calculator();
@@ -13,4 +14,39 @@ public class CalculatorTestSuite {
         int sumResult = calculator.sum(a, b);
         assertEquals(13, sumResult);
     }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int subtractResult = calculator.subtract(a, b);
+        assertEquals(-3, subtractResult);
+    }
+
+    @Test
+    public void testExponentiation1() {
+        Calculator calculator = new Calculator();
+        int a = 0;
+        int powerResult = calculator.exponentiation(a);
+        assertEquals(0, powerResult);
+    }
+
+    @Test
+    public void testExponentiation2() {
+        Calculator calculator = new Calculator();
+        int a = 3;
+        int powerResult = calculator.exponentiation(a);
+        assertEquals(9, powerResult);
+    }
+
+    @Test
+    public void testExponentiation3() {
+        Calculator calculator = new Calculator();
+        int a = -10;
+        int powerResult = calculator.exponentiation(a);
+        assertEquals(100, powerResult);
+    }
+
+
 }
--- kodilla-intro/src/main/java/aNauka.java
+++ kodilla-intro/src/main/java/aNauka.java
@@ -109,4 +109,16 @@ public class aNauka {
         return 0;
     }
 
+    static String getDayName(int chosenNumber) {
+            return switch (chosenNumber) {
+                case 1 -> "Monday";
+                case 2 -> "Tuesday";
+                case 3 -> "Wednesday";
+                case 4 -> "Thursday";
+                case 5 -> "Friday";
+                case 6 -> "Saturday";
+                case 7 -> "Sunday";
+                default -> "Wrong Number";
+            };
+    }
 }
