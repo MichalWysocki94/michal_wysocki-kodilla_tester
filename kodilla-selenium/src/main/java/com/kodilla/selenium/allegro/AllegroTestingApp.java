df1415108b60cc7a89e28052a25ebc306ea76efc
Michal Wysocki <michal.wysocki.ta@gmail.com>
2026-04-28 11:18:41 +0200

    Zadanie 13.3 Ebay selenium - poprawka_1

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -0,0 +1,29 @@
+package com.kodilla.selenium.allegro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+
+public class AllegroTestingApp {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("http://allegro.pl/");
+
+        WebElement inputField = driver.findElement(By.xpath("//button"));
+        inputField.submit();
+
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+//        WebElement yearCombo = driver.findElement(By.xpath("//*[@id=\"birthday_wrapper\"]/select[3]"));
+//        Select yearSelect = new Select(yearCombo);
+//        yearSelect.selectByIndex(5);
+    }
+}
--- kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -5,15 +5,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class EBayTestingApp {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.ebay.com");
 
-        WebElement inputField = driver.findElement(By.name("_nkw"));
-        inputField.sendKeys("Laptop");
-        inputField.submit();
+        try {
+            driver.get("https://www.ebay.com");
+
+
+            WebDriverWait wait = new WebDriverWait(driver, 20);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("_nkw")));
+            WebElement inputField = driver.findElement(By.name("_nkw"));
+            inputField.sendKeys("Laptop");
+            inputField.submit();
+        } finally {
+            driver.quit();
+        }
+
     }
 }
